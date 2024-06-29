eb2691ace6899bb18d2419e6260bdc630413ae1f
MaouOni <116459612+MaouOni@users.noreply.github.com>
2024-06-29 17:55:01 -0600

    First commit

---
+++ src/main/java/com/example/alumnosbackend/AlumnosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.alumnosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumnosBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlumnosBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/alumnosbackend/AlumnosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alumnosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumnosBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
