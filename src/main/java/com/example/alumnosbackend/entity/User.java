eb02a5d53b2a692d44513ac80a70729a27e5631e
MaouOni <116459612+MaouOni@users.noreply.github.com>
2024-06-30 09:37:07 -0600

    Some changes

--- src/main/java/com/example/alumnosbackend/entity/User.java
+++ src/main/java/com/example/alumnosbackend/entity/User.java
@@ -1,19 +1,18 @@
 package com.example.alumnosbackend.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users") // Changed table name to "users"
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String email;
+    private String materno;
     private String nombre;
     private String paterno;
-    private String materno;
-    private String email;
 
     // Getters and setters
     public Long getId() {
