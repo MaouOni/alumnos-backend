a25477fce22b69d92f19f31bc2ebc024f9bf1fe2
MaouOni <116459612+MaouOni@users.noreply.github.com>
2024-06-30 21:50:09 -0600

    New server

--- src/main/java/com/example/alumnosbackend/config/WebConfig.java
+++ src/main/java/com/example/alumnosbackend/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://alumnos-frontend.onrender.com/")
+                        .allowedOrigins("https://alumnos-frontend.onrender.com", "https://alumnos-frontend-primeng-build.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
